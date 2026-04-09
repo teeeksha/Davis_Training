72c89b19f07b5e64e7a6759158842642703845f9
Teeksha <teekshasharma364@gmail.com>
2026-04-09 09:34:32 +0530

    Update Q1__Bonus.py

--- Coding_Challenge /Q1__Bonus.py
+++ Coding_Challenge /Q1__Bonus.py
@@ -1,1 +1,7 @@
+#Question 1:
+#Scenario:A company gives bonuses based on employee salary.
+#Task:Write a Python program to calculate the bonus amount using given rules.
+salary = 45000
+bonus = salary * 0.07
+print("Bonus =", int(bonus))
 
